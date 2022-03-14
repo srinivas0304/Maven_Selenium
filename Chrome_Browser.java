60d4faeee9b811c8b01abface2a7715fc076e35a
A Srinivas <asrinivass021@gmail.com>
2022-03-14 11:03:52 +0530

    Chrome_Browser file added

---
+++ Chrome_Browser.java
@@ -0,0 +1,24 @@
+package launchBrowsers;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Chrome_Browser 
+{
+	WebDriver driver;
+	
+	@Test
+	public void openBrowser()
+	{
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+	}
+}
